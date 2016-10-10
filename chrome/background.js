58423f5f8c841504742fcf33d698d43f46e9d6be
jcpyun <johnchpyun@gmail.com>
2016-10-10 08:09:44 -0700

    simplifying for the final deployment

--- chrome/background.js
+++ chrome/background.js
@@ -1,24 +1,30 @@
-// var contextsList = ['selection','link','image','page'];
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.executeScript({
-        code: 'document.body.style.backgroundColor="red"'
-    });
-});
+// chrome.browserAction.onClicked.addListener(function(tab) {
+//     chrome.tabs.executeScript({
+//         code: 'document.body.style.backgroundColor="red"'
+//     });
+// });
 
-chrome.contextMenus.create({
-    id: "wow",
-    title: "ebay toolkit",
-    contexts: ["selection"],
-});
+// chrome.contextMenus.create({
+//     id: "wow",
+//     title: "ebay toolkit",
+//     contexts: ["selection"],
+// });
+
+
 
 
 // chrome.contextMenus.onClicked.addListener(function myFunction(selectedText) {
-//     alert(selectedText.selectionText);
+//     chrome.tabs.create({ url: "https://www.ebay.com/sch/" + selectedText.selectionText })
 // });
 
-chrome.contextMenus.onClicked.addListener(function myFunction(selectedText) {
-    chrome.tabs.create({ url: "https://www.ebay.com/sch/" + selectedText.selectionText })
-});
+// chrome.pageCapture.on
 
-chrome.pageCapture.on
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  // No tabs or host permissions needed!
+  console.log('Turning ' + tab.url + ' red!');
+  chrome.tabs.executeScript({
+    code: 'document.body.style.backgroundColor="#999999"'
+  });
+});
