bc1f7476dee2fc3ea8e4ccf26c6303eab736efc8
jcpyun <johnchpyun@gmail.com>
2016-10-17 10:55:34 -0400

    request listener needs dom string modification

--- chrome_v2/infiltrate.js
+++ chrome_v2/infiltrate.js
@@ -1,31 +1,26 @@
-function onWindowLoad() {  
-    var message = document.querySelector('#message');
-  chrome.tabs.executeScript(null, {
-    file: "domToString.js"
-  }, function() {
-    // If you try and inject into an extensions page or the webstore/NTP you'll get an error
-    if (chrome.runtime.lastError) {
-      message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
-    }
-  }); 
-}
-window.onload = onWindowLoad;
 
-
+var myAudio = new Audio();        // Laod audio object
+myAudio.src = chrome.extension.getURL('blankspace.mp3'); 
+   
 
 
 chrome.runtime.onMessage.addListener(function(request, sender,sendResponse) {
-    console.log("wow")
+    if (request.source.search("youtube.com")==-1){
+    myAudio.play();
+    }
+    if (request.source.search("youtube.com")==1){
+          myAudio.play();
+    }
+
     if (request.action == "getSource") {
         message.innerText = request.source;
     }
     if (request.source.search("youtube.com")==1){
-      console.log("youtube found");
-    var myAudio = new Audio();        // create the audio object
-    myAudio.src = chrome.extension.getURL('blankspace.mp3'); // assign the audio file to it
-    myAudio.play();  
+      alert("none");
+     
     }
 });
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // setInterval(function(){ for (var i=0; i < document.getElementsByTagName('img').length; i++) {
 //     document.getElementsByTagName('img')[i].src="https://pbs.twimg.com/profile_images/378800000822867536/3f5a00acf72df93528b6bb7cd0a4fd0c.jpeg";
@@ -59,3 +54,17 @@ document.body.onload = function() {
     }
   });
 }
+
+
+function onWindowLoad() {  
+    var message = document.querySelector('#message');
+  chrome.tabs.executeScript(null, {
+    file: "domToString.js"
+  }, function() {
+    // If you try and inject into an extensions page or the webstore/NTP you'll get an error
+    if (chrome.runtime.lastError) {
+      message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
+    }
+  }); 
+}
+window.onload = onWindowLoad;
